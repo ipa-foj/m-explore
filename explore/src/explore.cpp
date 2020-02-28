258155c9082d544aa6da3a345adcef5d8c34ef32
Max Beutelspacher <max.beutelspacher@ipa.fraunhofer.de>
2020-02-28 09:10:10 +0100

    disable debug output

--- explore/src/explore.cpp
+++ explore/src/explore.cpp
@@ -303,10 +303,10 @@ void Explore::stop()
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "explore");
-  if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
-                                     ros::console::levels::Debug)) {
-    ros::console::notifyLoggerLevelsChanged();
-  }
+  // if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
+  //                                    ros::console::levels::Debug)) {
+  //   ros::console::notifyLoggerLevelsChanged();
+  // }
   explore::Explore explore;
   ros::spin();
 
